b61d4de19930ee49c13ebdc5bbabe8470ed765ad
Lautaro Jara <lautarojra@gmail.com>
2026-05-14 16:45:38 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/functional-router/", // 👈 clave
+  build: {
+    outDir: "docs", // 👈 carpeta que va a leer GitHub Pages
+  },
+});
